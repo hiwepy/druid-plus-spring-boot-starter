13c47c8e2174551f3b9501dfd0348f33d6d18aa0
luffy <luffy@uzx.com>
2025-07-18 20:34:02 +0800

    3.3.x.20241003.RELEASE

--- src/test/java/com/alibaba/druid/spring/boot/DemoApplicationTests.java
+++ src/test/java/com/alibaba/druid/spring/boot/DemoApplicationTests.java
@@ -2,17 +2,14 @@ package com.alibaba.druid.spring.boot;
 
 import javax.sql.DataSource;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
