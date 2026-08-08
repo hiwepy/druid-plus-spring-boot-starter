10ecddfe9da0840cfbc0b3f8e9f6cd8eacc45600
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:08 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/main/java/com/alibaba/druid/spring/boot/DruidAutoConfiguration.java
+++ src/main/java/com/alibaba/druid/spring/boot/DruidAutoConfiguration.java
@@ -44,7 +44,7 @@ import com.alibaba.druid.wall.WallFilter;
 /**
  * DruidAutoConfiguration配置类，代替 DruidDataSourceAutoConfigure初始化Druid数据源，支持动态数据源
  * 记得在启动类添加@EnableAutoConfiguration(exclude={DruidDataSourceAutoConfigure.class})
- * @author 		： <a href="https://github.com/easy-4-java">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @Configuration
 @ConditionalOnClass(com.alibaba.druid.pool.DruidDataSource.class)
