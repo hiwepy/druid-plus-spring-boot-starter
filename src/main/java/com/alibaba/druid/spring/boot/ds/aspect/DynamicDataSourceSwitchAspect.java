579013e9a8a6d84285dcc030c8cd41d3c7073163
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:39:54 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.1.x branch

--- src/main/java/com/alibaba/druid/spring/boot/ds/aspect/DynamicDataSourceSwitchAspect.java
+++ src/main/java/com/alibaba/druid/spring/boot/ds/aspect/DynamicDataSourceSwitchAspect.java
@@ -12,7 +12,7 @@ import com.alibaba.druid.spring.boot.ds.annotation.SwitchRepository;
 
 /**
  * 数据源自动切换切面
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @Aspect
 @Component
