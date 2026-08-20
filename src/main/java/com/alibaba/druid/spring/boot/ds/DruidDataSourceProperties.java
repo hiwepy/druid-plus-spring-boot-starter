92dd04376202e3d881f1598110b6dec0cf54f19f
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-20 18:49:41 +0800

    sync: merge druid-metrics-prometheus into druid-plus-spring-boot-starter, upgrade Druid to 1.2.28, remove spring-enhance (inner DataSourceRoutingKeyHolder), align code with JDK requirements for Spring Boot 2.x/3.x/4.x matrix

--- src/main/java/com/alibaba/druid/spring/boot/ds/DruidDataSourceProperties.java
+++ src/main/java/com/alibaba/druid/spring/boot/ds/DruidDataSourceProperties.java
@@ -3,7 +3,7 @@ package com.alibaba.druid.spring.boot.ds;
 import java.util.List;
 import java.util.Properties;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
 import org.springframework.util.StringUtils;
 
 import com.alibaba.druid.pool.DruidAbstractDataSource;
@@ -13,6 +13,11 @@ import lombok.Data;
 
 @SuppressWarnings("serial")
 @Data
+/**
+ * <p>Auto-configuration for DruidDataSourceProperties.</p>
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
+ * @since 1.0.0
+ */
 public class DruidDataSourceProperties {
 
 	/** 基本属性 url、user、password */
@@ -156,10 +161,15 @@ public class DruidDataSourceProperties {
 	private String validationQuery = "SELECT 1";
 	private Integer validationQueryTimeout = -1;
 
+	/** @return return the db type. */
 	public String getDbType() {
 		return StringUtils.hasText(dbType) ? dbType : JdbcUtils.getDbType(this.getUrl(), null);
 	}
 
+	/**
+	 * <p>To properties.</p>
+	 * @return the result
+	 */
 	public Properties toProperties() {
 
 		Properties properties = new Properties();
@@ -174,6 +184,12 @@ public class DruidDataSourceProperties {
 		return properties;
 	}
 
+	/**
+	 * <p>Not null add.</p>
+	 * @param properties
+	 * @param key
+	 * @param value
+	 */
 	protected void notNullAdd(Properties properties, String key, Object value) {
 		if (value != null) {
 			properties.setProperty("druid." + key, value.toString());
@@ -181,6 +197,11 @@ public class DruidDataSourceProperties {
 	}
 	
 	
+	/**
+	 * <p>Configure properties.</p>
+	 * @param basicProperties
+	 * @return the result
+	 */
 	public DruidDataSourceProperties configureProperties(DataSourceProperties basicProperties) {
 		 //if not found prefix 'spring.datasource.druid' jdbc properties ,'spring.datasource' prefix jdbc properties will be used.
 		if (this.getName() == null) {
---
+++ src/test/java/com/alibaba/druid/spring/boot/ds/DynamicRoutingDataSourceTest.java
@@ -0,0 +1,208 @@
+package com.alibaba.druid.spring.boot.ds;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+/**
+ * Tests for {@link DynamicRoutingDataSource}.
+ *
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
+ */
+class DynamicRoutingDataSourceTest {
+
+    private DynamicRoutingDataSource dataSource;
+
+    @BeforeEach
+    void setUp() {
+        dataSource = new DynamicRoutingDataSource();
+    }
+
+    @Test
+    @DisplayName("DynamicRoutingDataSource can be instantiated")
+    void canBeInstantiated() {
+        assertThat(dataSource).isNotNull();
+    }
+
+    @Test
+    @DisplayName("getTargetDataSources returns map after setup")
+    void getTargetDataSources() {
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        DruidDataSource ds1 = new DruidDataSource();
+        ds1.setUrl("jdbc:h2:mem:test1");
+        targetDataSources.put("ds1", ds1);
+
+        DruidDataSource ds2 = new DruidDataSource();
+        ds2.setUrl("jdbc:h2:mem:test2");
+        targetDataSources.put("ds2", ds2);
+
+        dataSource.setTargetDataSources(targetDataSources);
+
+        DataSourceProperties basicProps = new DataSourceProperties();
+        basicProps.setUrl("jdbc:h2:mem:default");
+        dataSource.setDefaultTargetDataSource(new DruidDataSource());
+
+        // Need to set afterPropertiesSet to resolve
+        dataSource.afterPropertiesSet();
+
+        Map<Object, Object> result = dataSource.getTargetDataSources();
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("setNewTargetDataSources adds to existing targets")
+    void setNewTargetDataSources() {
+        Map<Object, Object> initialSources = new HashMap<>();
+        DruidDataSource ds1 = new DruidDataSource();
+        ds1.setUrl("jdbc:h2:mem:test1");
+        initialSources.put("ds1", ds1);
+
+        dataSource.setTargetDataSources(initialSources);
+        dataSource.setDefaultTargetDataSource(new DruidDataSource());
+        dataSource.afterPropertiesSet();
+
+        Map<Object, Object> newSources = new HashMap<>();
+        DruidDataSource ds2 = new DruidDataSource();
+        ds2.setUrl("jdbc:h2:mem:test2");
+        newSources.put("ds2", ds2);
+
+        dataSource.setNewTargetDataSources(newSources);
+
+        Map<Object, Object> result = dataSource.getTargetDataSources();
+        assertThat(result).hasSize(2);
+        assertThat(result).containsKey("ds1");
+        assertThat(result).containsKey("ds2");
+    }
+
+    @Test
+    @DisplayName("removeTargetDataSource removes a target")
+    void removeTargetDataSource() {
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        DruidDataSource ds1 = new DruidDataSource();
+        ds1.setUrl("jdbc:h2:mem:test1");
+        targetDataSources.put("ds1", ds1);
+
+        DruidDataSource ds2 = new DruidDataSource();
+        ds2.setUrl("jdbc:h2:mem:test2");
+        targetDataSources.put("ds2", ds2);
+
+        dataSource.setTargetDataSources(targetDataSources);
+        dataSource.setDefaultTargetDataSource(new DruidDataSource());
+        dataSource.afterPropertiesSet();
+
+        dataSource.removeTargetDataSource("ds1");
+
+        Map<Object, Object> result = dataSource.getTargetDataSources();
+        assertThat(result).hasSize(1);
+        assertThat(result).containsKey("ds2");
+        assertThat(result).doesNotContainKey("ds1");
+    }
+
+    @Test
+    @DisplayName("setTargetDataSource with name adds a new data source")
+    void setTargetDataSourceByName() {
+        Map<Object, Object> initialSources = new HashMap<>();
+        DruidDataSource defaultDs = new DruidDataSource();
+        defaultDs.setUrl("jdbc:h2:mem:default");
+
+        dataSource.setTargetDataSources(initialSources);
+        dataSource.setDefaultTargetDataSource(defaultDs);
+        dataSource.afterPropertiesSet();
+
+        DataSourceProperties basicProps = new DataSourceProperties();
+        basicProps.setUrl("jdbc:h2:mem:test");
+        basicProps.setUsername("sa");
+        basicProps.setPassword("");
+
+        DruidDataSourceProperties druidProps = new DruidDataSourceProperties();
+        druidProps.setUrl("jdbc:h2:mem:slave1");
+        druidProps.setUsername("sa");
+        druidProps.setPassword("");
+
+        dataSource.setTargetDataSource("slave1", basicProps, druidProps);
+
+        Map<Object, Object> result = dataSource.getTargetDataSources();
+        assertThat(result).containsKey("slave1");
+    }
+
+    @Test
+    @DisplayName("setTargetDataSource with properties uses name from druidProperties")
+    void setTargetDataSourceWithProperties() {
+        Map<Object, Object> initialSources = new HashMap<>();
+        DruidDataSource defaultDs = new DruidDataSource();
+        defaultDs.setUrl("jdbc:h2:mem:default");
+
+        dataSource.setTargetDataSources(initialSources);
+        dataSource.setDefaultTargetDataSource(defaultDs);
+        dataSource.afterPropertiesSet();
+
+        DataSourceProperties basicProps = new DataSourceProperties();
+        basicProps.setUrl("jdbc:h2:mem:test");
+        basicProps.setUsername("sa");
+        basicProps.setPassword("");
+
+        DruidDataSourceProperties druidProps = new DruidDataSourceProperties();
+        druidProps.setName("namedSlave");
+        druidProps.setUrl("jdbc:h2:mem:slave");
+        druidProps.setUsername("sa");
+        druidProps.setPassword("");
+
+        dataSource.setTargetDataSource(basicProps, druidProps);
+
+        Map<Object, Object> result = dataSource.getTargetDataSources();
+        assertThat(result).containsKey("namedSlave");
+    }
+
+    @Test
+    @DisplayName("setTargetDataSource copies basic properties when druid properties are null")
+    void setTargetDataSourceCopiesBasicProperties() {
+        Map<Object, Object> initialSources = new HashMap<>();
+        DruidDataSource defaultDs = new DruidDataSource();
+        defaultDs.setUrl("jdbc:h2:mem:default");
+
+        dataSource.setTargetDataSources(initialSources);
+        dataSource.setDefaultTargetDataSource(defaultDs);
+        dataSource.afterPropertiesSet();
+
+        DataSourceProperties basicProps = new DataSourceProperties();
+        basicProps.setUrl("jdbc:h2:mem:basic");
+        basicProps.setUsername("basicUser");
+        basicProps.setPassword("basicPass");
+        basicProps.setDriverClassName("org.h2.Driver");
+
+        DruidDataSourceProperties druidProps = new DruidDataSourceProperties();
+        druidProps.setName("copyTest");
+
+        dataSource.setTargetDataSource("copyTest", basicProps, druidProps);
+
+        Map<Object, Object> result = dataSource.getTargetDataSources();
+        assertThat(result).containsKey("copyTest");
+    }
+
+    @Test
+    @DisplayName("afterPropertiesSet registers keys in DataSourceRoutingKeyHolder")
+    void afterPropertiesSetRegistersKeys() {
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        DruidDataSource ds1 = new DruidDataSource();
+        ds1.setUrl("jdbc:h2:mem:test1");
+        targetDataSources.put("ds1", ds1);
+
+        dataSource.setTargetDataSources(targetDataSources);
+        dataSource.setDefaultTargetDataSource(new DruidDataSource());
+        dataSource.afterPropertiesSet();
+
+        // afterPropertiesSet should have been called, registering keys
+        assertThat(dataSource.getTargetDataSources()).containsKey("ds1");
+    }
+}
